7d93912e9a09d9c02f921a4a08e242539a5cd5d5
AmanZulkifli <Amans172007@gmail.com>
2025-12-04 14:05:15 +0700

    Navbar fixing

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,13 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {},
+theme: {
+  extend: {
+    colors: {
+      primary: "#2563eb", // or your exact blue
+      primaryHover: "#3b82f6", 
+    },
   },
+},
   plugins: [],
 };
